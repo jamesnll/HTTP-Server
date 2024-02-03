0447d362063df7cdf84799b14e0900e27f3c41cf
jamesnll <langillejames11@gmail.com>
2024-02-03 00:32:02 -0800

    Updated the enum for additional status codes

--- include/server.h
+++ include/server.h
@@ -44,8 +44,11 @@ struct kv_pair
 
 enum status_codes
 {
-    OK        = 200,
-    NOT_FOUND = 404
+    OK                    = 200,
+    CREATED               = 201,
+    BAD_REQUEST           = 400,
+    NOT_FOUND             = 404,
+    INTERNAL_SERVER_ERROR = 500
 };
 
 void run_server(const struct arguments *args);
